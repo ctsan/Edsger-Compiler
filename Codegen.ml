16f32326aab2fd7b1229f43154a36c70a47b670c
angelakis <a.angelakis@outlook.com>
2016-09-28 03:01:03 +0300

    same

--- Codegen.ml
+++ Codegen.ml
@@ -440,7 +440,7 @@ and ins_of_quad qd =
     let st_ins  = store Rax qd.quad_argZ in
     let type_size = qd.quad_argX |> type_of_operand |> (*deref_expr |>*) sizeOfType  in 
     ld_ins @
-    [I_mov (Const(Imm8 type_size),Reg (Rcx,B16))] @
+    [I_movw (Const(Imm8 type_size),Reg (Rcx,B16))] @
     (* Offset goes to RCX *)
     [I_imul (Reg(Rax,B16), Reg (Rcx, B16)) ] @ (* TODO double check imul has the correct format *)
     (* address goes to RAX*)
